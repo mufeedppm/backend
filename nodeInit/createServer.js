93d3a2c6f86dd38a9ef2cddfda24169af6b84c05
mufeedppm <mufeedppm@gmail.com>
2023-02-05 19:49:15 +0530

    added responses

--- nodeInit/createServer.js
+++ nodeInit/createServer.js
@@ -1,10 +1,21 @@
 const http= require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log("name");
-    console.log(req)  
+    if(req.url=='/home'){
+        res.write('<html><header><title>home page</title></header><body>Welcome home</body></html>')
+    }
+    else if(req.url=='/about'){
+        res.write('<html><header><title>about us</title></header><body>Welcome to About Us</body></html>')
+    }
+    else if(req.url=='/node'){
+        res.write('<html><header><title>node js</title></header><body>Welcome to my Node js Project</body></html>')
+    }
+    
+      
 });
 
 
 server.listen(4000);
+
+
 
