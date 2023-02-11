2e45e08d8df9c619dc4188bb689788648b9df783
mufeedppm <mufeedppm@gmail.com>
2023-02-12 02:31:56 +0530

    group chat app using express &localstorage

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.get("/login", (req, res) => {
+    res.send(
+      `<form action="/login" method="POST" onSubmit="localStorage.setItem('username', document.getElementById('username').value)">
+          <input type="text" name="username" placeholder="username" id="username">
+          <br />
+          <button type="submit">Login</button>
+      </form>`
+    );
+  });
+  
+  app.post("/login", (req, res) => {
+    res.redirect("/");
+  });
+  
+app.get("/", (req, res) => {
+  fs.readFile("message.txt", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(
+      `${data}<form action="/" method="POST" onSubmit="document.getElementById('username').value = localStorage.getItem('username')">
+                <input type="text" name="message" id="message">
+                <input type="hidden" name="username" id="username">
+                <br />
+                <button type="submit">Send</button>
+            </form>`
+    );
+  });
+});
+
+app.post("/", (req, res) => {
+  fs.appendFile(
+    "message.txt",
+    ` | ${req.body.username}: ${req.body.message}`,
+    
+    (err) => (err ? console.log(err) : null)
+  );
+
+  res.redirect("/");
+});
+
+
+app.listen(3000);
